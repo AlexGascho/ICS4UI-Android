a7830d870ffdc5591732f13d9bdeadde34e9c222
Alex-Gascho <gasca4686@wrdsb.ca>
2022-05-12 18:03:12 -0400

    it builds now

--- app/src/main/java/com/example/ics4ui_app/CalendarDaySpacing.java
+++ app/src/main/java/com/example/ics4ui_app/CalendarDaySpacing.java
@@ -7,27 +7,27 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import java.util.ArrayList;
 
-public class CalendarDaySpacing extends RecyclerView.Adapter<CalendarViewHolder>
-{
-    private final ArrayList<String> cellDayText;
-
-    public CalendarAdapter(ArrayList<String> cellDayText) {
-        this.cellDayText = cellDayText;
-    }
-
-    @NonNull
-    @Override
-    public CalendarViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
-    }
-
-    @Override
-    public void onBindViewHolder(@NonNull CalendarViewHolder holder, int position) {
-
-    }
-
-    @Override
-    public int getItemCount() {
-        return 0;
-    }
-}
+//public class CalendarDaySpacing extends RecyclerView.Adapter<CalendarViewHolder>
+//{
+//    private final ArrayList<String> cellDayText;
+//
+//    public CalendarAdapter(ArrayList<String> cellDayText) {
+//        this.cellDayText = cellDayText;
+//    }
+//
+//    @NonNull
+//    @Override
+//    public CalendarViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+//        return null;
+//    }
+//
+//    @Override
+//    public void onBindViewHolder(@NonNull CalendarViewHolder holder, int position) {
+//
+//    }
+//
+//    @Override
+//    public int getItemCount() {
+//        return 0;
+//    }
+//}
---
+++ app/src/main/java/com/example/ics4ui_app/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.ics4ui_app;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.view.View;
+
+import com.example.ics4ui_app.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+    private ActivityMainBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+    }
+}
