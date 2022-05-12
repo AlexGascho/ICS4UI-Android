6d6a33a7c2f12a0d3e9ca114562f343c71df257d
Elliot2373 <104775965+Elliot2373@users.noreply.github.com>
2022-05-12 10:47:39 -0400

    4:02 in video (lol)

---
+++ app/src/main/java/com/example/ics4ui_app/CalendarViewHolder.java
@@ -0,0 +1,13 @@
+package com.example.ics4ui_app;
+
+import android.view.View;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class CalendarViewHolder extends RecyclerView.ViewHolder {
+
+    public CalendarViewHolder(@NonNull View itemView) {
+        super(itemView);
+    }
+}
