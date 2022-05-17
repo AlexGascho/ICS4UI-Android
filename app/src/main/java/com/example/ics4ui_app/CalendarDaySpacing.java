5266bc90aed2fcb3ec6ac21037a631506a76ccb0
Bit-Owen <owenmarshall@4917.ca>
2022-05-17 09:57:01 -0400

    Revert "Merge pull request #3 from AlexGascho/Elliot-and-Owens-god-code"
    This reverts commit 058f1f68e88b1c16dcece2cab1b18832d0b7a518, reversing
    changes made to 32efc99ac8dcd4caa6ebeb61069d027c6a70968b.

--- app/src/main/java/com/example/ics4ui_app/CalendarDaySpacing.java
+++ app/src/main/java/com/example/ics4ui_app/CalendarDaySpacing.java
@@ -1,7 +1,5 @@
 package com.example.ics4ui_app;
 
-import android.view.LayoutInflater;
-import android.view.View;
 import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
@@ -9,37 +7,27 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import java.util.ArrayList;
 
-public class CalendarDaySpacing extends RecyclerView.Adapter<CalendarViewHolder>
-{
-    private final ArrayList<String> daysOfMonth;
-    private final OnItemListener onItemListener;
-
-    public CalendarDaySpacing(ArrayList<String> daysOfMonth, OnItemListener onItemListener) {
-        this.daysOfMonth = daysOfMonth;
-        this.onItemListener = onItemListener;
-    }
-
-    @NonNull
-    @Override
-    public CalendarViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        LayoutInflater inflater = LayoutInflater.from(parent.getContext());
-        View view = inflater.inflate(R.layout.calendar_cell, parent, false);
-        ViewGroup.LayoutParams layoutParams = view.getLayoutParams();
-        layoutParams.height = (int) (parent.getHeight() * 0.166666666);
-        return new CalendarViewHolder(view, onItemListener);
-    }
-
-    @Override
-    public void onBindViewHolder(@NonNull CalendarViewHolder holder, int position) {
-
-        holder.dayOfMonth.setText(daysOfMonth.get(position));
-    }
-
-    @Override
-    public int getItemCount() {
-        return daysOfMonth.size();
-    }
-    public interface OnItemListener{
-        void onItemClick(int position, String dayText);
-    }
-}
+//public class CalendarDaySpacing extends RecyclerView.Adapter<CalendarViewHolder>
+//{
+//    private final ArrayList<String> cellDayText;
+//
+//    public CalendarAdapter(ArrayList<String> cellDayText) {
+//        this.cellDayText = cellDayText;
+//    }
+//
+//    @NonNull
+//    @Override
+//    public CalendarViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+//        return null;
+//    }
+//
+//    @Override
+//    public void onBindViewHolder(@NonNull CalendarViewHolder holder, int position) {
+//
+//    }
+//
+//    @Override
+//    public int getItemCount() {
+//        return 0;
+//    }
+//}
