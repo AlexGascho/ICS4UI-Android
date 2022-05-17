d4eb91dade5e48ca6cff640eb28162156c3beec8
Elliot2373 <104775965+Elliot2373@users.noreply.github.com>
2022-05-17 09:55:26 -0400

    Revert "Merge pull request #3 from AlexGascho/Elliot-and-Owens-god-code"
    This reverts commit 058f1f68e88b1c16dcece2cab1b18832d0b7a518, reversing
    changes made to 32efc99ac8dcd4caa6ebeb61069d027c6a70968b.

--- app/src/main/java/com/example/ics4ui_app/CalendarView.java
+++ app/src/main/java/com/example/ics4ui_app/CalendarView.java
@@ -1,90 +1,64 @@
 package com.example.ics4ui_app;
 
-import android.os.Build;
 import android.os.Bundle;
 
-import androidx.annotation.RequiresApi;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.recyclerview.widget.GridLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
+import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 
-import java.time.LocalDate;
-import java.time.Year;
-import java.time.YearMonth;
-import java.time.format.DateTimeFormatter;
-import java.util.ArrayList;
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link CalendarView#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class CalendarView extends Fragment {
 
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
 
-public class CalendarView extends AppCompatActivity implements CalendarDaySpacing.OnItemListener{
-    private TextView monthYearText;
-    private RecyclerView calendarRecyclerView;
-    private LocalDate selectedDate;
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
 
-    @RequiresApi(api = Build.VERSION_CODES.O)
+    public CalendarView() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment CalendarView.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static CalendarView newInstance(String param1, String param2) {
+        CalendarView fragment = new CalendarView();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.fragment_calendar_view);
-        initWidgets();
-        selectedDate = LocalDate.now();
-        setMonthView();
-
-    }
-
-    @RequiresApi(api = Build.VERSION_CODES.O)
-    private void setMonthView() {
-        monthYearText.setText(monthYearFromDate(selectedDate));
-        ArrayList<String> daysInMonth = daysInMonthArray(selectedDate);
-
-        CalendarDaySpacing calendarDaySpacing = new CalendarDaySpacing(daysInMonth, this);
-        RecyclerView.LayoutManager layoutManager = new GridLayoutManager()
-    }
-
-    @RequiresApi(api = Build.VERSION_CODES.O)
-    private ArrayList<String> daysInMonthArray(LocalDate date) {
-        ArrayList<String> daysInMonthArray = new ArrayList<>();
-        YearMonth yearMonth = YearMonth.from(date);
-        int daysInMonth = yearMonth.lengthOfMonth();
-
-        LocalDate firstOfMonth = selectedDate.withDayOfMonth(1);
-        int dayOfWeek = firstOfMonth.getDayOfWeek().getValue();
-
-        for(int i = 0; i <= 42; i++){
-            if(i <= dayOfWeek || i > daysInMonth + dayOfWeek){
-                daysInMonthArray.add(" ");
-            }
-            else {
-                daysInMonthArray.add(String.valueOf(i + dayOfWeek));
-            }
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
         }
-        return daysInMonthArray;
-    }
-
-    @RequiresApi(api = Build.VERSION_CODES.O)
-    private String monthYearFromDate(LocalDate date){
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MMMM yyyy");
-        return date.format(formatter);
-    }
-
-    private void initWidgets() {
-        calendarRecyclerView = findViewById(R.id.calendarRecyclerView);
-        monthYearText = findViewById(R.id.monthYearTextView);
-    }
-
-    public void previousMonthAction(View view){
-
-    }
-    public void nextMonthAction(View view){
-
     }
 
     @Override
-    public void onItemClick(int position, String dayText) {
-
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_calendar_view, container, false);
     }
 }
