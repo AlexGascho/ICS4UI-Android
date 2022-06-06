178471bfe69e8bf18db1ee615b52176abad6e4c1
Alex-Gascho <gasca4686@wrdsb.ca>
2022-06-05 23:53:15 -0400

    fixed calendar

--- app/src/main/java/com/ics4ui/android/CalendarFragment.java
+++ app/src/main/java/com/ics4ui/android/CalendarFragment.java
@@ -1,15 +1,33 @@
 package com.ics4ui.android;
 
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+
+import com.ics4ui.android.databinding.FragmentCalendarBinding;
 
 
 public class CalendarFragment extends Fragment {
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+
+    private FragmentCalendarBinding binding;
+
+    public CalendarFragment() {
 
     }
 
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        binding = FragmentCalendarBinding.inflate(inflater, container, false);
+        return binding.getRoot();
+    }
+
+    public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
+
+        super.onViewCreated(view, savedInstanceState);
+    }
 }
