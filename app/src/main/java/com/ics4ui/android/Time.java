67c531c4ffc5f787cc927de431bb1ab17d467e7f
TheoHarder <104775992+TheoHarder@users.noreply.github.com>
2022-06-06 10:06:45 -0400

    added more methods to replace bad code

--- app/src/main/java/com/ics4ui/android/Time.java
+++ app/src/main/java/com/ics4ui/android/Time.java
@@ -7,6 +7,47 @@ public class Time {
     public int hour;
     public int minute;
     public String nameOfMonth;
+
+    public int getDay() {
+        return day;
+    }
+
+    public void setDay(int day) {
+        this.day = day;
+    }
+
+    public int getMonth() {
+        return month;
+    }
+
+    public void setMonth(int month) {
+        this.month = month;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public int getHour() {
+        return hour;
+    }
+
+    public void setHour(int hour) {
+        this.hour = hour;
+    }
+
+    public int getMinute() {
+        return minute;
+    }
+
+    public void setMinute(int minute) {
+        this.minute = minute;
+    }
+
     static void getDayOfWeek(){
 
     }
